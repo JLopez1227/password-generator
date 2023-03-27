516a5f0d460fb7a8b2d2823701141b0c6093ea73
JLopez1227 <josuenmilopez27@gmail.com>
2023-03-27 18:29:26 -0500

    Added vars, if statements, window.confirm, to get the password generator fully functioning. Also started working in the README file.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# password-generator
+# Password-generator
+
+## 
+In this assignment starter code to a password generator was given to me. It was my task to implment JavaScript code to build a functioning Password Generator that gives you a secure password. Using JavaScript I added window prompts with criteria of what the user would like their password to include. I added vars of the possible characters that can be used in your password. I also added a copy button so that when the user's password is generated they can copy their new secure password.
+
--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 // Assignment code here
-var lowercase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-var uppercase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, Q, Y, Z]
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'x', 'z']
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8,9,]
 var specialCharacters = ['!', '"', '#', '#', '%', '&', "'", '(', ')', '*', '-', '.', '/', '\\', ';', '<', '>', '=', '+', '?', '@', '{', '}', '/', '^', '_', '`', '[', ']', '|', '~']
+var passwordLength = 12;
+var password = ""
+var choices = []
 
 function getPasswordOp(){
-  var length = parseInt(prompt("How many characters long would you like your password to be?"));
+  choices = []
+ passwordLength = window.prompt("How many characters long would you like your password to be (between 8-128)?");
+  var includeUpper = window.confirm("Would you like your password to include upper cased letters?")
+  if (includeUpper) {
+    choices= choices.concat(uppercase)
+  }
+  var includeLower = window.confirm ("Would you like your password to include lower cased letters?")
+  if (includeLower) {
+    choices= choices.concat(lowercase)
+  }
+  var includeNumbers = window.confirm ("Would you like for your password to include numbers?")
+  if (includeNumbers) {
+    choices= choices.concat(numeric)
+  }
+  var includeSpecial = window.confirm("Would you like your password to include special characters?")
+  if (includeSpecial) {
+    choices= choices.concat(specialCharacters)
+  }
+}  
 
-}  10 
+function generatePassword(){
+  password = ""
+  getPasswordOp()
+  var passArray = []
 
+
+  for (var i = 0; i <passwordLength; i++){
+    var randomNumber = Math.floor(Math.random()* choices.length);
+    passArray.push(choices[randomNumber]);
+  }
+  password = passArray.join('');
+  return password;
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -17,7 +50,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
